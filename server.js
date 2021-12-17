2b27f180717ee82670e1bf10d3ee9dd1a8f07811
Jason Simpson <jsimpson@integral.com.au>
2021-12-17 14:02:24 +1000

    server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 const express = require('express')
-const { Nuxt } = require('nuxt')
+const consola = require('consola')
+const { loadNuxt } = require('nuxt-start')
 const app = express()
-const host = process.env.HOST || '127.0.0.1'
-const port = process.env.PORT || 3000
-// Import and set Nuxt.js options
-const config = require('./nuxt.config.js')
-const nuxt = new Nuxt(config)
-// Give Nuxt middleware to express
-app.use(nuxt.render)
-// Listen the server
-app.listen(port, host)
-console.log('Server listening on ' + host + ':' + port)
+
+async function start() {
+  const nuxt = await loadNuxt(isDev ? 'dev' : 'start')
+  await nuxt.listen(process.env.PORT, process.env.HOST)
+}
+
+start()
